ccac2558359cc762c6463cc76f535f7f967d3402
Manan Shah <manan.shah.777@gmail.com>
2015-04-10 09:22:09 -0700

    Add function addPlot
    Adds a plot to a given xlsx file provided the function used to generate
    the plot

--- R/xlsxboost.R
+++ R/xlsxboost.R
@@ -90,8 +90,8 @@ xlsx.openFile<-function(filename = NULL)
 #' wb <- createWorkbook(type="xlsx")
 #' sheet <- xlsx::createSheet(wb, sheetName = "example")
 #' xlsx.addHeader(wb, sheet, value="Header 1",level=1, color="black")
-#' saveWorkbook(wb, "examples_add_header.xlsx")
-#' xlsx.openFile("examples_add_header.xlsx") # view the file
+#' saveWorkbook(wb, "example.xlsx")
+#' xlsx.openFile("example.xlsx") # view the file
 #' 
 xlsx.addHeader<-function(wb, sheet, value="Header", level=1, color="#FFFFFF",
                          startRow=NULL, startCol=2)
@@ -100,17 +100,17 @@ xlsx.addHeader<-function(wb, sheet, value="Header", level=1, color="#FFFFFF",
     color="white" # black and white are inverted in package 'xlsx'
   
   H1 <- CellStyle(wb) + 
-              Font(wb,  heightInPoints=22, color=color, isBold=TRUE, underline=0)
+    Font(wb,  heightInPoints=22, color=color, isBold=TRUE, underline=0)
   H2 <- CellStyle(wb) + 
-              Font(wb, heightInPoints=18, color=color, isItalic=FALSE, isBold=TRUE, underline=0)
+    Font(wb, heightInPoints=18, color=color, isItalic=FALSE, isBold=TRUE, underline=0)
   H3 <- CellStyle(wb) + 
-              Font(wb, heightInPoints=16, color=color, isItalic=TRUE, isBold=TRUE, underline=0)
+    Font(wb, heightInPoints=16, color=color, isItalic=TRUE, isBold=TRUE, underline=0)
   H4 <- CellStyle(wb) + 
-              Font(wb, heightInPoints=16, color=color, isItalic=TRUE, isBold=FALSE, underline=0)
+    Font(wb, heightInPoints=16, color=color, isItalic=TRUE, isBold=FALSE, underline=0)
   H5 <- CellStyle(wb) + 
-              Font(wb, heightInPoints=14, color=color, isItalic=TRUE, isBold=FALSE, underline=0)
+    Font(wb, heightInPoints=14, color=color, isItalic=TRUE, isBold=FALSE, underline=0)
   H6 <- CellStyle(wb) + 
-              Font(wb, heightInPoints=12, color=color, isItalic=TRUE, isBold=FALSE, underline=0)
+    Font(wb, heightInPoints=12, color=color, isItalic=TRUE, isBold=FALSE, underline=0)
   
   # Now, append the row to the sheet
   if(is.null(startRow)){
@@ -129,4 +129,52 @@ xlsx.addHeader<-function(wb, sheet, value="Header", level=1, color="#FFFFFF",
   else if(level==4) xlsx::setCellStyle(sheetTitle[[1,1]], H4)
   else if(level==5) xlsx::setCellStyle(sheetTitle[[1,1]], H5)
   else if(level==6) xlsx::setCellStyle(sheetTitle[[1,1]], H6)  
+}
+
+
+#' Adds a plot to a .xlsx file
+#' 
+#' Makes use of numerous parameters including the sheet, text, start row/column, and 
+#' width/height in order to display a plot in an excel workbook provided a plot function.
+#' 
+#' @param wb The excel workbook
+#' @param sheet The excel sheet (created by xlsxboost)
+#' @param plotFunction The function (i.e. ggplot2(...)) to show in Excel
+#' @param startRow The row to start the header on
+#' @param startCol The column to start the header on
+#' @param width The width of the plot
+#' @param height The height of the plot
+#' 
+#' @export
+#' @examples
+#' 
+#' wb <- createWorkbook(type="xlsx")
+#' sheet <- xlsx::createSheet(wb, sheetName = "example")
+#' x = seq(from = -10*pi, to = 10*pi, by = 0.1)
+#' pf <- function() {plot(sin(x))}
+#' xlsx.addPlot(wb, sheet, pf)
+#' saveWorkbook(wb, "example2.xlsx")
+#' xlsx.openFile("example2.xlsx") # view the file
+#' 
+xlsx.addPlot<-function(wb, sheet, plotFunction, startRow=NULL, startCol=2,
+                       width=480, height=480, ...)
+  
+{
+  # Step 1: plot and create the PNG
+  png(filename = "plot.png", width = width, height = height,...)
+  plotFunction()
+  dev.off() 
+  
+  # Append plot to sheet if startRow is not specified
+  if(is.null(startRow)){
+    rows <- getRows(sheet)
+    startRow = length(rows) + 1
+  } 
+  
+  # Add the file created previously
+  addPicture("plot.png", sheet=sheet,  startRow = startRow, startColumn = startCol) 
+  
+  # Status
+  res <- file.remove("plot.png")
+  res
 }
